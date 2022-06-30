dacbdb9d021d2a2d7022ce853f6e523c5ec4516f
ysaad01 <ysaad2325@gmail.com>
2022-06-30 01:07:40 -0400

    set up mutations and JWT authentication logic

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { authMiddleware } = require("./utils/auth");
 // import Apolloserver
 const { ApolloServer } = require("apollo-server-express");
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 const app = express();
