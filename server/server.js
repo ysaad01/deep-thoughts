59132dcc9fde26404f7d8497fc48e1fb7de63b05
ysaad01 <ysaad2325@gmail.com>
2022-07-04 18:01:47 -0400

    implemented client-side GraphQL query for homepage data

--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const { authMiddleware } = require("./utils/auth");
-// import Apolloserver
 const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
 
-//import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
-//create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
 });
 
 const app = express();
@@ -20,10 +17,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
-  //integrate our Apollo server with Express application as middleware
   server.applyMiddleware({ app });
 
   db.once("open", () => {
